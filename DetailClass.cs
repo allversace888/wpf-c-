1090442390fb585fd9888bf329de7f4782150fb5
allversace888 <128177410+allversace888@users.noreply.github.com>
2023-03-26 00:55:51 +0300

    Add files via upload

---
+++ DetailClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sql
+{
+    public class DetailClass
+    {
+            public int id_preparation { get; set; }
+            public string drug_name { get; set; }
+            public int price { get; set; }
+            public int amount { get; set; }
+            public string total_price { get; set; }
+    }
+}
